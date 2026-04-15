9d79ceadf3f185b12d0005aa5ae1e643520ea92b
Alexander Bukharov <alexander.bukharov@spirent.com>
2026-04-15 22:12:18 +0700

    init empty spring project

---
+++ src/test/java/com/bukharov/credit_app/CreditAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bukharov.credit_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
